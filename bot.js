40bf2fa6927c08f3c1712f9b274f709c9b40c30e
lithium <louis.cabrol@student.ecp.fr>
2018-02-17 12:37:03 +0100

    check for empty add and lint

--- bot.js
+++ bot.js
@@ -13,22 +13,25 @@ const addItem = (text) => {
   return db.newItem(item);
 };
 
-bot.start((ctx) => {
-  console.log('started:', ctx.from.id);
-  return ctx.reply('Welcome!');
-});
+bot.start(ctx => ctx.reply('Welcome!'));
 bot.command('add', (ctx) => {
   const text = ctx.message.text
     .split(' ')
     .slice(1)
     .join(' ');
+  if (!text) {
+    ctx.reply('No ToDo item ?');
+    return;
+  }
   addItem(text)
     .then(ctx.replyWithMarkdown(`Added item \`${text}\``))
     .catch(err => ctx.reply(`An error occured ${err}`));
 });
 bot.command('get', (ctx) => {
-  db.getItems.then((docs) => {
-    docs.forEach(item => ctx.reply(item.text));
-  });
+  db.getItems
+    .then((docs) => {
+      docs.forEach(item => ctx.reply(item.text));
+    })
+    .catch(err => ctx.reply(`An error occured ${err}`));
 });
 bot.startPolling();
